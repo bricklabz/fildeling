25524e3362e53cf56ce2371a79fdee0c2aee7102
Stefan Dannenberger <s.dannenberger@gmail.com>
2025-10-06 23:15:40 +0200

    Første versjon av filene med mytools som verktøykasse og main som brukes for å sjekke at alt virker fint. Visual Studio Code må åpne src-mappen for at det skal fungere!

---
+++ src/main.py
@@ -0,0 +1,8 @@
+import mytools
+
+print("Here we go!")
+
+mytools.printHW()
+mytools.printGB()
+
+print(mytools.a * mytools.b)
---
+++ src/mytools.py
@@ -0,0 +1,11 @@
+def printHW():
+    print("Hello World")
+
+def printGB():
+    print("Goodbye!")
+
+a = 2
+b = 3
+
+
+print("Hello there - all set!")
